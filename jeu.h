326c665d16d98d0cb521a872dddb3519aa22c993
Thiziri <thizirirahali2@gmail.com>
2025-12-03 15:27:19 +0100

    Dépot de base

---
+++ jeu.h
@@ -0,0 +1,27 @@
+#ifndef JEU_H
+#define JEU_H
+
+typedef struct 
+{
+    
+}Carte;
+
+typedef struct 
+{
+    
+    /* data */
+
+}Joueur;
+
+typedef struct
+{
+
+    /* data */
+
+}Rangee ;
+
+
+
+
+
+#endif
