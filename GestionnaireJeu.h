e7101013b4af5fd32551a734a73bbec530c16024
JereMP4 <56267758+JereMP4@users.noreply.github.com>
2025-12-15 18:48:47 +0100

    Fix GestionnaireJeu.h : changer le type de retour de void à int pour GestionnaireJeu_poserCarte

--- GestionnaireJeu.h
+++ GestionnaireJeu.h
@@ -14,10 +14,10 @@ typedef struct {
     Collection colonnes[4];
 } GestionnaireJeu;
 
-/* “méthodes” */
+/* "méthodes" */
 void GestionnaireJeu_Init(GestionnaireJeu *p, char *ip, int port);
 void GestionnaireJeu_AjouteJoueur(GestionnaireJeu *p, Joueur *j);
 
-void GestionnaireJeu_poserCarte(GestionnaireJeu *p, int colonne, Carte *c);
+int GestionnaireJeu_poserCarte(GestionnaireJeu *p, int colonne, Carte *c);
 
 #endif
