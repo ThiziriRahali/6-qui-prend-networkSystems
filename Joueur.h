c7c9f5e131b58aaf543d516786b64c85217d7383
JereMP4 <56267758+JereMP4@users.noreply.github.com>
2025-12-16 17:40:50 +0100

    Remove comments from Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -10,12 +10,11 @@ typedef struct {
     char nom[MAX_CHARS];
     Collection jeuCartes;
     int score;
-    int socket;           // Socket pour communication réseau (-1 pour bot)
-    int is_bot;           // 1 si c'est un bot, 0 si humain
-    int id;               // ID du joueur (index)
+    int socket;
+    int is_bot;
+    int id;
 } Joueur;
 
-/* "éthodes" */
 void Joueur_Init(Joueur *p, int x, char* y);
 void Joueur_Method(Joueur *p, int dx, char* dy);
 
